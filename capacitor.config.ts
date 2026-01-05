28ceca70c0f3f916a7c0e53168ec99134987d7aa
lauraandrelaa <laura.andrela@gdbr-tg.com.br>
2026-01-05 10:33:11 -0300

    Picking
    --> Liberação API http

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'pickinglist-AJUSTEPOPUP',
+  appId: 'picking.list',
+  appName: 'pickinglist',
   webDir: 'dist'
 };
 
