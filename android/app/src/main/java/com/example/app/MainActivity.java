94ad938536327ad442567aa5e199cf7451d36ca0
raisidoro <depto.ti04@gdbr-tg.com.br>
2026-01-05 21:58:20 -0300

    Picking List - Ajustado para Produção
    --> Realização de ajustes para funcionamento do Picking List em ambiente de produção.
    ATENÇÃO: para não sobrescrever os arquivos ajustados da pasta android, segue passo a passo em caso de necessidade de ajustes:
    
    --> Caso sejam alterações no frontend, parte de desenvolvimento em si:
    1. NÃO apaga a pasta android, mantém como esta.
    2. Roda NPM run build
    3. Roda NPX cap copy android
    4. Roda NPM cap open android
    5. No Android Studio clica nas quatro linhazinhas do canto (ao lado de "android") pra expandir
    6. No caminho Build > Generate App Bundles or APKs, seleciona Generate APKs.
    
    --> Caso sejam alterações em plugins/bibliotecas do CAPACITOR APENAS:
    1. NÃO apaga a pasta android, mantém como esta.
    2. Roda NPX cap sync android
    3. Roda NPM cap open android
    4. No Android Studio clica nas quatro linhazinhas do canto (ao lado de "android") pra expandir
    5. No caminho Build > Generate App Bundles or APKs, seleciona Generate APKs.
    
    Obs.: Normalmente vai gerar o apk na pasta android\app\build\outputs\apk\debug. Pega o arquivo app-debug.apk pra instalar e pronto :)

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.app;
+package com.example.app;  // ← SEU PACKAGE real
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,25 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'pickinglist-AJUSTEPOPUP',
-  webDir: 'dist'
+  appId: 'com.example.app',  
+  appName: 'PickingList',
+  webDir: 'dist',
+
+  server: {
+    androidScheme: 'http',
+    cleartext: true
+  },
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0
+    }
+  },
+
+  android: {
+    allowMixedContent: true,
+    webContentsDebuggingEnabled: true  
+  }
 };
 
 export default config;
